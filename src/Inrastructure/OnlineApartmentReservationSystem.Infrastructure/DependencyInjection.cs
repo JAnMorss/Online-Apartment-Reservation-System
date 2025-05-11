27f8a9f8cc26d829088ba2fa24b65d6c06731983
JAnMorss <johnanthonymorales124@gmail.com>
2025-05-11 16:36:22 +0800

    Configuring Dependency Injection

--- src/Inrastructure/OnlineApartmentReservationSystem.Infrastructure/DependencyInjection.cs
+++ src/Inrastructure/OnlineApartmentReservationSystem.Infrastructure/DependencyInjection.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dapper;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OnlineApartmentReservationSystem.Application.Abstractions.Clock;
+using OnlineApartmentReservationSystem.Application.Abstractions.Data;
 using OnlineApartmentReservationSystem.Application.Abstractions.Email;
+using OnlineApartmentReservationSystem.Domain.Apartments.Interface;
+using OnlineApartmentReservationSystem.Domain.Bookings.Repositories;
+using OnlineApartmentReservationSystem.Domain.Users.Interface;
 using OnlineApartmentReservationSystem.Infrastructure.Clock;
+using OnlineApartmentReservationSystem.Infrastructure.Data;
 using OnlineApartmentReservationSystem.Infrastructure.Email;
+using OnlineApartmentReservationSystem.Infrastructure.Repositories;
+using OnlineApartmentReservationSystem.Shared.Abstractions.Domain;
 
 namespace OnlineApartmentReservationSystem.Infrastructure
 {
@@ -26,6 +34,16 @@ namespace OnlineApartmentReservationSystem.Infrastructure
             {
                 options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
             });
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IApartmentRepository, ApartmentRepository>();
+            services.AddScoped<IBookingRepository, BookingRepository>();
+            services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+            services.AddSingleton<ISqlConnectionFactory>(_ => 
+                new SqlConnectionFactory(connectionString));
+
+            SqlMapper.AddTypeHandler(new DateOnlyTypeHandler());
 
             return services;
         }
